cb28171cfe99966f55c244ed74661d18c61b0ab3
KEITH L ROBINSON <krobnson@Keiths-MBP.hsd1.mn.comcast.net>
2018-02-07 21:20:59 -0600

    set defaults to model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -13,7 +20,8 @@
     popularised in the 1960s with the release
     of Letraset sheets containing Lorem Ipsum passages,
     and more recently with desktop publishing software
-    like Aldus Page"
+    like Aldus Page",
+    topic_id: Topic.last.id
   )
 end
 puts " 10 blog posts created"
@@ -27,10 +35,10 @@ end
 
 puts " 5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "RoR",
     body: "Lorem Ipsum is simply dummy text
     of the printing and typesetting industry.
     Lorem Ipsum has been the industry's standard
@@ -49,4 +57,25 @@ puts " 5 skills created"
   )
 end
 
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "React javascript",
+    body: "Lorem Ipsum is simply dummy text
+    of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard
+    dummy text ever since the 1500s, when an
+    unknown printer took a galley of type and
+    scrambled it to make a type specimen book.
+    It has survived not only five centuries,
+    but also the leap into electronic typesetting,
+    remaining essentially unchanged. It was
+    popularised in the 1960s with the release
+    of Letraset sheets containing Lorem Ipsum passages,
+    and more recently with desktop publishing software
+    like Aldus Page",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
 puts " 9 portfolio items created"
